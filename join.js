e0767ab239230bc051c9e70f4ceee9f7de42d15e
McChug <mcchug2112@gmail.com>
2024-02-21 20:33:52 -0700

    lobby improvements

--- join.js
+++ join.js
@@ -25,6 +25,16 @@ function joinGame() {
     pin = pin.toUpperCase();
    
     // validate username and pin, set to localStorage, and run enterLobby()
+    if (username.length === 0) {
+        const nameLabel = document.getElementById("nameLabel");
+        nameLabel.style.color = "#ec2227";
+        setTimeout(() => { nameLabel.style.color = "#fcdebd" }, 750);
+    }
+    if (!pins.includes(pin)) {
+        const nameLabel = document.getElementById("pinLabel");
+        nameLabel.style.color = "#ec2227";
+        setTimeout(() => { nameLabel.style.color = "#fcdebd" }, 750);
+    }
     if (username.length > 0 && pins.includes(pin)) {
         localStorage.setItem('username', username);
         localStorage.setItem('pin', pin);
@@ -35,8 +45,8 @@ function joinGame() {
 function enterLobby(username, pin) {
     // create main and footer content
     //******websocket will populate this next line with each player, max of 8
-    const playerCards = `<div class="player-card">${username}</div>` 
-    const main = `<main style="width:100%;">${playerCards}<button class="reflection" style="width:20%;margin-top:2rem;" onclick="runGame()">Start Game!</button></main>`
+    const playerCards = `<div class="player-card">${username}</div><div class="player-card">placeholder</div><div class="player-card">placeholder</div><div class="player-card">placeholder</div><div class="player-card-empty"><div class="empty"></div></div><div class="player-card"></div><div class="player-card"></div><div class="player-card"></div>` 
+    const main = `<main style="width:100%;gap:2rem;">${playerCards}<button class="reflection" style="width:20%;margin-top:2rem;" onclick="runGame()">Start Game!</button></main>`
     const footer = `<footer><nav><menu><li><a>${username}</a></li><li><a>${pin}</a></li></menu></nav></footer>`
     const body = document.getElementById("body");
     body.innerHTML = `${main}${footer}<script src="play.js"></script>`;

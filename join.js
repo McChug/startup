7835ef96920a9e6f46113e238c0f291afdebb1ea
McChug <mcchug2112@gmail.com>
2024-02-21 18:49:13 -0700

    started enterLobby()

--- join.js
+++ join.js
@@ -1,9 +1,10 @@
 
 const pins = []
-//replace with database connection
-pins.push('ssss')
+//******replace with database connection
+pins.push('SSSS')
 
 function testIfInGame() {
+    // test if username and pin exist in localStorage and go in-game if so
     const username = localStorage.getItem('username');
     const pin = localStorage.getItem('pin')
 
@@ -13,13 +14,17 @@ function testIfInGame() {
         console.log('not in game');
     }
 }
+// always call function when page is loaded
 testIfInGame()
 
 function joinGame() {
-    const username = document.getElementById("name").value;
-    const pin = document.getElementById("pin").value;
-    // console.log(username);
-    // console.log(pin);
+    // set username and pin based off form
+    let username = document.getElementById("name").value;
+    let pin = document.getElementById("pin").value;
+    username = username.toUpperCase();
+    pin = pin.toUpperCase();
+   
+    // validate username and pin, set to localStorage, and run enterLobby()
     if (username.length > 0 && pins.includes(pin)) {
         localStorage.setItem('username', username);
         localStorage.setItem('pin', pin);
@@ -28,6 +33,10 @@ function joinGame() {
 }
 
 function enterLobby(username, pin) {
+    // create main and footer content
+    const playerCards = `<div class="player-card">${username}</div>`
+    const main = `<main style="width:100%;">${playerCards}<button class="reflection" style="width:20%;margin-top:2rem;">Start Game!</button></main>`
+    const footer = `<footer><nav><menu><li><a>${username}</a></li><li><a>${pin}</a></li></menu></nav></footer>`
     const body = document.getElementById("body");
-    body.innerHTML = '<p>test</p>';
+    body.innerHTML = `${main}${footer}`;
 }

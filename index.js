5e30ac4691a21efb80c06b612e8638e5084c190b
McChug <mcchug2112@gmail.com>
2024-03-19 18:10:51 -0600

    mongo tests

--- index.js
+++ index.js
@@ -6,8 +6,44 @@ const https = require('https');
 require('dotenv').config();
 
 const { MongoClient } = require('mongodb');
-const dbUrl = `mongodb+srv://${process.env.usernameDB}:${process.env.passwordDB}@${process.env.hostnameDB}`
-const client = new MongoClient(dbUrl);
+
+async function main() {
+    const dbUrl = `mongodb+srv://${process.env.usernameDB}:${process.env.passwordDB}@${process.env.hostnameDB}`
+    const client = new MongoClient(dbUrl);
+    const db = client.db('rental');
+    const collection = db.collection('house');
+
+//     // Test that you can connect to the database
+//     (async function testConnection() {
+//         await client.connect();
+//         await db.command({ ping: 1 });
+//     })().catch((ex) => {
+//         console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+//         process.exit(1);
+//     });
+
+//     // Insert a document
+//     const house = {
+//         name: 'Beachfront views',
+//         summary: 'From your bedroom to the beach, no shoes required',
+//         property_type: 'Condo',
+//         beds: 1,
+//     };
+//     await collection.insertOne(house);
+
+//     // Query the documents
+//     const query = { property_type: 'Condo', beds: { $lt: 2 } };
+//     const options = {
+//         sort: { score: -1 },
+//         limit: 10,
+//     };
+
+//     const cursor = collection.find(query, options);
+//     const rentals = await cursor.toArray();
+//     rentals.forEach((i) => console.log(i));
+// }
+
+main().catch(console.error);
 
 
 // service port

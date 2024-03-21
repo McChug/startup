3975616b098a89a2da332cbdf2585903ce30ba16
McChug <mcchug2112@gmail.com>
2024-03-21 17:43:28 -0600

    login.js added to

--- public/login.js
+++ public/login.js
@@ -1,1 +1,30 @@
 const username = localStorage.getItem('username');
+
+async function loginPlayer() {
+    setUser('/auth/login');
+}
+
+async function createPlayer() {
+    setUser('/auth/create');
+}
+
+async function setUser(endpoint) {
+    const username = document.querySelector('#name')?.value;
+    const password = document.querySelector('#pw')?.value;
+    const response = await fetch(endpoint, {
+        method: 'post',
+        body: JSON.stringify({ email: username, password: password}),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        }
+    });
+
+    if (response.ok) {
+        localStorage.setItem('username', username);
+        console.log('Logged in!');
+    } else {
+        const errorMsg = await response.json();
+        const msgDiv = document.getElementById('loginMsg');
+        msgDiv.textContent = `Error: ${errorMsg}`;
+    };
+}

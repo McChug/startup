4c69a47fc314b28a025f53877a355e1ffa3959ec
McChug <mcchug2112@gmail.com>
2024-03-21 18:58:01 -0600

    database.js created in backend

---
+++ database.js
@@ -0,0 +1,48 @@
+const { MongoClient } = require('mongodb');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
+require('dotenv').config();
+
+const dbUrl = `mongodb+srv://${process.env.usernameDB}:${process.env.passwordDB}@${process.env.hostnameDB}`
+const client = new MongoClient(dbUrl);
+const db = client.db('rental');
+const playerCollection = db.collection('player');
+
+// test connection
+(async function testConnection() {
+    await client.connect();
+    await db.command({ ping: 1 });
+})().catch((ex) => {
+    console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+    process.exit(1);
+});
+
+function getPlayer(email) {
+    return playerCollection.findOne({ email: email });
+}
+
+function getPlayerByToken(token) {
+    return playerCollection.findOne({ token: token });
+}
+
+async function createNewPlayer(email, password) {
+    // encrypt the password first
+    const pwHash = await bcrypt.hash(password, 10);
+
+    const player = {
+        email: email,
+        password: pwHash,
+        token: uuid.v4(),
+    };
+
+    await playerCollection.insertOne(player);
+
+    return player
+}
+
+module.exports = {
+    getPlayer,
+    getPlayerByToken,
+    createNewPlayer,
+    createNewPlayer
+};

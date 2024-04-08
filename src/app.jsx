add61b223a8bbcb5158d018ceb96176b1511dcea
McChug <mcchug2112@gmail.com>
2024-04-08 17:58:11 -0600

    css for basic react

--- src/app.jsx
+++ src/app.jsx
@@ -2,5 +2,24 @@ import React from 'react';
 import './app.css';
 
 export default function App() {
-  return <div className='body bg-dark text-light'>App will display here</div>;
+  return (
+    <div className='main-page'>
+        <main>
+            <img alt="logo" src="mva_logo.svg" />
+            <p>App components will go here</p>
+        </main>
+
+        <footer>
+            <nav>
+                <menu>
+                    <li><a href="rules.html">How to Play</a></li>
+                    <li><a href="openlobby.html">Open Lobby</a></li>
+                    <li><a href="https://github.com/McChug/startup">GitHub</a></li>
+                </menu>
+            </nav>
+        </footer>
+        <script src="join.js"></script>
+        <script src="play.js"></script>
+    </div>
+  );
 }

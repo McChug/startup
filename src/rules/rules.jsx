d1df0012c0f0fd83e74b2b202448d6fe8bdd1036
McChug <mcchug2112@gmail.com>
2024-04-09 18:32:53 -0600

    rules component connects back to app

--- src/rules/rules.jsx
+++ src/rules/rules.jsx
@@ -1,48 +1,46 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './rules.css';
 
 export function Rules() {
     return (
-    <div className='main-page'>
-        <header>
-            <h1>HOW TO PLAY</h1>
-        </header>
+        <div className='main-page'>
+            <header>
+                <h1>HOW TO PLAY</h1>
+            </header>
 
-        <Routes>
-        <Route path='/' element={<Start />} exact />
-        <main>
-            <h2>ALIEN INVASION?!</h2>
-            <p>In Mixtapes vs. Aliens, form a team of 3-8 ambassadors to give these aliens a reason not to take over. The only thing is . . . we can only comminucate with music.</p>
+            <main>
+                <h2>ALIEN INVASION?!</h2>
+                <p>In Mixtapes vs. Aliens, form a team of 3-8 ambassadors to give these aliens a reason not to take over. The only thing is . . . we can only comminucate with music.</p>
 
-            <h2 id="setup">SETUP</h2>
-            <p>On the <a href="index.html">main page,</a> hit "Open Lobby" at the bottom to create a new room. Let your fellow ambassadors know the 4-letter code to they can enter too. When you're ready to begin, hit the "Start" button!</p>
-        
-            <h2 id="rounds">ROUNDS</h2>
-            <p style={{marginBottom: '1.5rem' }}>Each round goes a little something like this:</p> 
-            <ol type="I">        
-                <li>One player is designated as ambassador.</li> 
-                <li>They select which alien will give an inquiry.</li>
-                <li>Each player is presented the inquiry on their device.</li>
-                <li>Putting the their headphones, they'll search for a song and note a timestamp.</li>
-                <li>After submitting the info, each response to shuffled and spat back to the players.</li>
-                <li>The ambassador is played each song and selects which one to share to the alien.</li>
-            </ol>        
+                <h2 id="setup">SETUP</h2>
+                <p>On the <NavLink to='start'>main page,</NavLink> hit "Open Lobby" at the bottom to create a new room. Let your fellow ambassadors know the 4-letter code to they can enter too. When you're ready to begin, hit the "Start" button!</p>
+            
+                <h2 id="rounds">ROUNDS</h2>
+                <p style={{marginBottom: '1.5rem' }}>Each round goes a little something like this:</p> 
+                <ol type="I">        
+                    <li>One player is designated as ambassador.</li> 
+                    <li>They select which alien will give an inquiry.</li>
+                    <li>Each player is presented the inquiry on their device.</li>
+                    <li>Putting the their headphones, they'll search for a song and note a timestamp.</li>
+                    <li>After submitting the info, each response to shuffled and spat back to the players.</li>
+                    <li>The ambassador is played each song and selects which one to share to the alien.</li>
+                </ol>        
 
-            <h2 id="scoring">SCORING</h2>
-            <p>Rounds end with the winning player receiving a space cookie. After each player has served two terms as ambassador, the player with the most space cookies wins!</p>
+                <h2 id="scoring">SCORING</h2>
+                <p>Rounds end with the winning player receiving a space cookie. After each player has served two terms as ambassador, the player with the most space cookies wins!</p>
 
-            <h2 id="extra-features">EXTRA FEATURES</h2>
-            <p>Like the songs you heard? Tap the link at the end of a game to turn them into a playlist.
-            <br />Prepping a party, download and share an MvA invite from the "Open Lobby" page.</p>
-        </main>
-        </Routes>
-
-        <footer>
-            <nav>
-                <menu>
-                    <li><NavLink className='nav-link' to='start'>Return</NavLink></li>
-                </menu>
-            </nav>
-        </footer>
-    </div>
+                <h2 id="extra-features">EXTRA FEATURES</h2>
+                <p>Like the songs you heard? Tap the link at the end of a game to turn them into a playlist.
+                <br />Prepping a party, download and share an MvA invite from the "Open Lobby" page.</p>
+            </main>
+            <footer>
+                <nav>
+                    <menu>
+                        <li><NavLink className='nav-link' to='/'>Return</NavLink></li>
+                    </menu>
+                </nav>
+            </footer>
+        </div>
     );
 }
